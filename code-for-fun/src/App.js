020694c2bbe2569a3f70cc4d4912ef2049460d88
rhornedo <115593654+rhornedo@users.noreply.github.com>
2023-08-30 22:17:06 -0400

    Added components and pages folders

--- code-for-fun/src/App.js
+++ code-for-fun/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/NavBar/index1';
+import { BrowserRouter as Router, Routes, Route }
+from 'react-router-dom';
+import About from './pages/about';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path='/pages/about' element={<About />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ code-for-fun/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1>
+				Code for Fun is a website that is dedicated to helping people learn.
+			</h1>
+		</div>
+	);
+};
+
+export default About;
